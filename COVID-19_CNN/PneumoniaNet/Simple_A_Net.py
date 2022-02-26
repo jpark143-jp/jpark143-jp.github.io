71fd2c335612d5b6f46cfa5e292c72badcd1d55b
jpark143-jp <jpark143@g.ucla.edu>
2022-02-26 11:23:13 -0800

    Update Simple_A_Net.py

--- COVID-19_CNN/PneumoniaNet/Simple_A_Net.py
+++ COVID-19_CNN/PneumoniaNet/Simple_A_Net.py
@@ -22,9 +22,6 @@ class Simple_A_Net:
         shape=(depth,height,width)
         channelDim=1
 
-
-
-
     model.add(Conv2D(4 , kernel_size=(3,3) , strides = 3 , padding = 'same' , activation = 'relu' , input_shape = shape, kernel_regularizer=l2(0.01)))
     model.add(BatchNormalization())
     model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
@@ -45,16 +42,6 @@ class Simple_A_Net:
     model.add(Dense(units = 32, activation = 'relu'))
     model.add(Dropout(0))
     model.add(Dense(units = 2, activation = 'sigmoid'))
-    model.compile(optimizer = 'adam' , loss = 'binary_crossentropy' , metrics = ['accuracy', 'binary_crossentropy'])
-    
-    #model.summary()
-    #learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience = 2, verbose=1,factor=0.3, min_lr=0.000001)
+ 
 
-    #history = model.fit(datagen.flow(x_train,y_train, batch_size = 32) ,epochs = 12 , validation_data = datagen.flow(x_val, y_val) ,callbacks = [learning_rate_reduction])
-    
-    
-    filepath = "pneumonia_classification.hdf5"
-    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=2, save_best_only=True, mode='min', patience=3)
-    callbacks_list = [checkpoint]
-    
     return model
