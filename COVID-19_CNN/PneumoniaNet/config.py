3f1841c7b31d1d1d15ce290d7203a08763e1d498
jpark143-jp <jpark143@g.ucla.edu>
2022-02-26 11:38:04 -0800

    Create config.py

---
+++ COVID-19_CNN/PneumoniaNet/config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_PATH = os.path.abspath("/Users/jpark143g.ucla.edu/Downloads/chest_xray/chest_xray")
+
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "train"])
+VAL_PATH = os.path.sep.join([BASE_PATH, "val"])
+TEST_PATH = os.path.sep.join([BASE_PATH, "test"])
